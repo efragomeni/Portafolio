e756c02d45efaa277b28ab2583829bbc5b38ac1c
efragomeni <efragomeni@gmail.com>
2025-11-19 15:24:17 -0300

    cambios esteticos

--- src/components/Proyectos/Proyectos.jsx
+++ src/components/Proyectos/Proyectos.jsx
@@ -10,6 +10,7 @@ export const Proyectos = () => {
       className={`contenedor-proyectos-individual ${
         invertido ? "invertido" : ""
       }`}
+      style={{ "--bg-proyecto": `url(${proyecto.imagen})` }}
     >
       <div className="contenedor-proyectos-individual-detalle-detalle">
         <h3>{proyecto.nombre}</h3>
@@ -31,25 +32,6 @@ export const Proyectos = () => {
     <section id="mis-proyectos">
       <div className="contenedor-proyectos">
         <h1>Mis proyectos</h1>
-        {/* <div className="contenedor-proyectos-individual">
-          <div className="contenedor-proyectos-individual-detalle-detalle">
-            <h3>{proyectos[0].nombre}</h3>
-            <p>{proyectos[0].descripcion}</p>
-            <a
-              href="https://github.com/efragomeni?tab=repositories"
-              target="_blank"
-            >
-              Github <img src="./src/assets/img/arrow.svg" alt="" />
-            </a>
-          </div>
-          <div className="contenedor-proyectos-individual-img">
-            <img src={proyectos[0].imagen} alt="" />
-          </div>
-        </div> */}
-        {/* {proyectos.map((p, i) => (
-          <ProyectoCard key={i} proyecto={p} />
-        ))} */}
-
         {proyectos.map((p, i) => (
           <ProyectoCard key={i} proyecto={p} invertido={i % 2 !== 0} />
         ))}
